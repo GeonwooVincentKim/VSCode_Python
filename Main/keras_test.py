8c8e41d9c5c0ffeec705f7a98974f678f0460411
GeonwooVincentKim <kdsnop@gmail.com>
2020-11-16 19:41:07 +0900

    Test

---
+++ Main/keras_test.py
@@ -0,0 +1,6 @@
+import keras
+print(keras.__version__)
+print(keras.preprocessing)
+
+from keras.preprocessing import image as image_utils
+print(image_utils)
--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,2 @@
-print("Hello")
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
